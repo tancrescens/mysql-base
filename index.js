b03f10c4e1bd856f3f0c8a52fe3c4d575612681d
crescens tan <tancrescens@gmail.com>
2024-12-24 06:15:56 +0000

    Initialization

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+// require in handlebars and their helpers
+const helpers = require('handlebars-helpers');
+
+let app = express();
+
+// app.set('view engine', 'hbs');
+// app.use(express.static('public'));
+// app.use(express.urlencoded({extended:false}));
+
+// wax.on(hbs.handlebars);
+// wax.setLayoutPath('./views/layouts');
+
+
+// // tell handlebars-helpers where to find handlebars
+// helpers({
+//     'handlebars': hbs.handlebars
+// })
+
+app.get('/', (req,res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is running')
+});
