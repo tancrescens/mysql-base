ca7e1bd831125f90fe03551c4b348a4a26984378
crescens tan <tancrescens@gmail.com>
2024-12-26 07:13:45 +0000

    Display Customers + respetive companies on a new hbs

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ wax.setLayoutPath('./views/layouts');
 // tell handlebars-helpers where to find handlebars
 helpers({
     'handlebars': hbs.handlebars
-})
+});
 
 let connection;
 
@@ -28,12 +28,38 @@ async function main() {
         'user': process.env.DB_USER,
         'database': process.env.DB_NAME,
         'password': process.env.DB_PASSWORD
-    })
+    });
 
+    // ROUTE: Default
     app.get('/', (req,res) => {
         res.send('Hello, World!');
     });
 
+    // ROUTE: Render one row / customer
+    app.get('/customers', async (req, res) => {
+        const [customers] = await connection.execute({
+            'sql':`
+            SELECT * from Customers
+                JOIN Companies ON Customers.company_id = Companies.company_id;
+            `,
+            nestTables: true
+
+        });        res.render('customers/index', {
+            'customers': customers
+        })
+        console.log(customers)
+    });
+    
+    // ROUTE: create customers in Customers DB
+    app.get("/customers/create", async (req,res)=>{
+        let companies = await connection.execute(`SELECT * from Companies`);
+        res.render("customers/add", {
+            "companies" : companies
+        });
+    });
+
+    
+    // END
     app.listen(3000, ()=>{
         console.log('Server is running')
     });
